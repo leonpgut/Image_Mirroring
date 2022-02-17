3ab49918b8921dccb0ba00a0c47103b156ea451f
st166379 <st166379@stud.uni-stuttgart.de>
2022-02-17 21:02:34 +0100

    Fix for rescaling. The window now scales to be the size of the screen at most, while the saved image keeps the original size.

--- Image_Mirroring.py
+++ Image_Mirroring.py
@@ -1,15 +1,18 @@
+from imp import new_module
 from PIL import Image, ImageOps, ImageTk
 import tkinter as tk
 from tkinter import filedialog as fd
 import sys
 from functools import partial
+from screeninfo import get_monitors
+import math
 
 
 def getpath():
     print("Choose an image to be mirrored.")
     rot = tk.Tk()
     rot.withdraw()
-    files = [('Image', ['*.png', '*.jpg', "*.jpeg"])]
+    files = [('Image', ['*.png', '*.jpg', "*.jpeg", "*.svg", "*.ppm", "*.webp", "*.gif"])]
     name = fd.askopenfilename(filetypes = files, defaultextension = files)
     rot.destroy()
     return name
@@ -18,7 +21,7 @@ def checkfile(path):
     if not path:
         sys.exit("Didn't choose a file.")
     filetype = path.split('.')[-1].lower()
-    possiblefiles = ["png", "jpg", "jpeg"]
+    possiblefiles = ["png", "jpg", "jpeg", "svg", "ppm", "webp", "gif"]
     if not filetype in possiblefiles:
         sys.exit("File not supported. Try png or jpg.")
     
@@ -28,7 +31,7 @@ def on_main_click(canvas, rect, event):
         x, y = event.x, event.y
         if x > 1 and x < w - 2:
             canvas.coords(rect, x-2, 0, x+2, h)
-            posi = x
+            posi = x/new_w
         
 
 def click(canvas, rect, event):
@@ -50,11 +53,13 @@ def quit(root, event):
     name = path.split('/')[-1]
     ml = mirrorleft(img)
     mr = mirrorright(img)
+    new_ml = mirrorleft(new_img)
+    new_mr = mirrorright(new_img)
     print("Mirrored " + name + " successfully.")
     root.destroy()
-    showresults(ml, mr)
+    showresults(new_ml, new_mr, ml, mr)
 
-def showresults(left, right):
+def showresults(left, right, le, ri):
     root = tk.Tk()
     root.title("Image Mirroring")
     root.focus_force()
@@ -70,9 +75,9 @@ def showresults(left, right):
     canvas.create_image(left.size[0], 0, anchor=tk.NW, image=r)
     frame = tk.Frame(root, width = w, height = 60)
     frame.pack()
-    butl = tk.Button(frame, text="Save left", command=partial(saveas, left))
+    butl = tk.Button(frame, text="Save left", command=partial(saveas, le))
     butl.place(x=0, y=0, height=60, width=left.size[0])
-    butr = tk.Button(frame, text="Save right", command=partial(saveas, right))
+    butr = tk.Button(frame, text="Save right", command=partial(saveas, ri))
     butr.place(x=left.size[0], y=0, height=60, width=right.size[0])
     root.mainloop()
 
@@ -80,27 +85,27 @@ def left_key(canvas, rect, event):
     global posi, w
     l = canvas.coords(rect)[0]
     r = canvas.coords(rect)[2]
-    if posi > 2:
+    if posi*new_w > 2:
         canvas.coords(rect, l-1, 0, r-1, h)
-        posi -= 1
+        posi -= 1/new_w
 
 def right_key(canvas, rect, event):
     global posi
     l = canvas.coords(rect)[0]
     r = canvas.coords(rect)[2]
-    if posi < w - 3:
+    if posi*new_w < new_w - 3:
         canvas.coords(rect, l+1, 0, r+1, h)
-        posi += 1
+        posi += 1/new_w
 
 def mirrorleft(img):
-    h = img.size[1]
-    lefthalf = img.crop([0, 0, posi, h])
+    w, h = img.size
+    lefthalf = img.crop([0, 0, posi*w, h])
     lefthalfmir = ImageOps.mirror(lefthalf)
     return concatenate(lefthalf, lefthalfmir)
 
 def mirrorright(img):
     w, h = img.size
-    righthalf = img.crop([posi, 0, w, h])
+    righthalf = img.crop([posi*w, 0, w, h])
     righthalfmir = ImageOps.mirror(righthalf)
     return concatenate(righthalfmir, righthalf)
 
@@ -138,27 +143,42 @@ def gui(img):
     root = tk.Tk()
     root.title("Image Mirroring")
     root.focus_force()
-    root.geometry(str(w) + "x" + str(h + 60))
+    root.geometry(str(new_w) + "x" + str(new_h + 60))
     root.resizable(0,0)
-    canvas = tk.Canvas(root, width = w, height = h)      
+    canvas = tk.Canvas(root, width = new_w, height = new_h)      
     canvas.pack()   
-    tkimage = ImageTk.PhotoImage(master=canvas, image=img)
+    tkimage = ImageTk.PhotoImage(master=canvas, image=new_img)
     canvas.create_image(0,0, anchor=tk.NW, image=tkimage)
-    rect = canvas.create_rectangle(posi-2, 0, posi+2, h, fill="black", outline="white")
+    rect = canvas.create_rectangle(posi*new_w-2, 0, posi*new_w+2, new_h, fill="black", outline="white")
     bind_events(root, canvas, rect)
-    frame = tk.Frame(root, width = w, height = 60)
+    frame = tk.Frame(root, width = new_w, height = 60)
     frame.pack()
     but = tk.Button(frame, text="Confirm", command=partial(quit, root, "Button press"))
-    but.place(x=0, y=0, height=60, width=w)
+    but.place(x=0, y=0, height=60, width=new_w)
     root.mainloop()
+
+def resize(img):
+    rot = tk.Tk()
+    rot.withdraw()
+    screenwidth = rot.winfo_screenwidth()
+    screenheight = rot.winfo_screenheight()
+    if 2*w > screenwidth or h + 60 > screenheight:
+        resize_factor = min(screenwidth/(2*w), screenheight/(h + 60))
+    new_w = math.floor(resize_factor * w)
+    new_h = math.floor(resize_factor * h)
+    new_size = (new_w, new_h)
+    new_img = img.resize(new_size)
+    rot.destroy() 
+    return resize_factor, new_w, new_h, new_img
 
 
 path = getpath()
 checkfile(path)
 img = Image.open(path).convert("RGBA")
 w, h = img.size
-posi = w/2
-move, grab = False, False
+resize_factor, new_w, new_h, new_img = resize(img)
+posi = 1/2
+move = False
 gui(img)
 
 
